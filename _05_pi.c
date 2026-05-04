69a0d10508c1a1cb0fe1f659e7353f0559eb168b
dombanushka15 <dombanushka@gmail.com>
2026-05-04 17:45:26 +0530

    adding new

---
+++ _05_pi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    
+    float pi = 3.14159;
+
+    // Print value of pi
+    printf("Value of Pi = %f", pi);
+
+    return 0;
+}
---
+++ _06_area_of_circle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float radius, area;
+    
+    printf("Enter radius of circle: ");
+    scanf("%f", &radius);
+
+
+    area = 3.14 * radius * radius;
+
+    printf("Area of circle = %f", area);
+
+    return 0;
+}
